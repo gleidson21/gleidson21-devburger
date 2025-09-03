7333a3783259af4d3e1691a2b3012b102cea7c55
gledson <gledsonalcantara4@gmail.com>
2025-09-03 18:55:27 -0400

    atualizei baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://devburger-api.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
